bc3d444d7f2fdddf04a722e4ce115110a1844e47
MrIohan <joao.mello.dev369@gmail.com>
2025-03-05 21:22:04 -0400

    atualização

--- app/Http/Controllers/Sales/SaleController.php
+++ app/Http/Controllers/Sales/SaleController.php
@@ -7,5 +7,33 @@ use Illuminate\Http\Request;
 
 class SaleController extends Controller
 {
-    //
+    public function index(){
+
+        return view("pages.sale");
+
+    }
+
+    public function create(){
+
+    }
+
+    public function store(SizeRequest $request){
+
+    }
+
+    public function show(string $id){
+
+    }
+
+    public function edit(string $id){
+
+    }
+
+    public function update(Request $request, string $id){
+
+    }
+
+    public function destroy(string $id){
+
+    }
 }
