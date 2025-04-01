c9eb7b0eaa7451a4ba9cad7b470342c9b5a09e0b
Juaokkkk <jaoalfrdo@gmail.com>
2025-04-01 11:19:31 -0300

    tela de vendas completa (front end)

--- resources/views/pages/sale.blade.php
+++ resources/views/pages/sale.blade.php
@@ -3,33 +3,40 @@
 
 @section('content')
 
-<div class="container-form">
-
-    <div class="box-form mini">
-
-<form>
-    @csrf
-
-    <select>
-        <option>Vendedor</option>
-    </select>
-
-    <select>
-        <option>Cliente</option>
-    </select>
-
-    <select>
-        <option>Adicionar Produto</option>
-    </select>
-
-    <label></label>
-    <input>
-
-    <button type="submit">Enviar</button>
-</form>
-
+<div class="setor setor-imagens">
+    <h3>Imagens dos Produtos</h3>
+    <div class="produtos-imagem-container">
+        <img src="\assets/img/produto1.jpg" alt="Produto 1">
+        <img src="\assets/img/produto2.jpg" alt="Produto 2">
+        <img src="\assets/img/produto3.jpg" alt="Produto 3">
+    </div>
 </div>
+<div class="container-sale">
+    <div class="setor setor-esquerdo">
+        <div class="logo-sale">
+            <img src="\assets/img/Lynarts.png" alt="">
+        </div>
+        <div class="valor-total">
+            <h2>Total: R$ 0,00</h2>
+        </div>
+    </div>
 
-</div>
+    <div class="setor setor-central">
+        <form>
+            <input type="text" placeholder="Campo 1">
+            <input type="text" placeholder="Campo 2">
+            <input type="text" placeholder="Campo 3">
+            <input type="text" placeholder="Campo 4">
+        </form>
+    </div>
 
+    <div class="setor setor-direito">
+        <div class="historico">
+            <h3>Histórico</h3>
+            <p>Produto 1</p>
+            <p>Produto 2</p>
+            <p>Produto 3</p>
+        </div>
+    </div>
+    
 @endsection
